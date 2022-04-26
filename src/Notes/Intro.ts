4a1e543595e62399d754f53503014c78e8525688
Venkat <vbonam@kent.edu>
2022-04-27 02:03:16 +0530

    update intro

--- src/Notes/Intro.ts
+++ src/Notes/Intro.ts
@@ -1,2 +1,13 @@
 // Angular Notes //
 
+-TypeScript:
+
+- it is a JavaScript + some additional features
+    - it is strongly typed
+        - declaring the type of the variable while declaring.
+- it is object oriented
+    - it follow the oops principles
+        - it is a complied language
+            - throws errors at compiled time
+
+
