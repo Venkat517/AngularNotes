60b2466158e2012852203b7c5616681de384a542
Venkat517 <vbonam@kent.edu>
2022-04-29 00:44:56 +0530

    Update Directives.js

--- src/Notes/Directives.js
+++ src/Notes/Directives.js
@@ -1,1 +1,23 @@
+//Directives in Angular//
+
+- Directives are the basic building blocks of elements which can change the apperance and behaviour of the elements.
+- Directives are the instructions placed in the DOM, where we can add the DOM element or remove DOM element.
+
+// Types of directives//
+
+- There are 3 different kinds of directives
+
+1)Component Directive
+    - These are used in the main class
+    - They contains the details how component should be processed
+    
+2)Structual Directives
+    - These are used to mainpulate and change the structure
+    - These are preccded with * sign
+    - eg ngIF, ngFor
+
+3)Attribure Directives
+    - These are used to change the look and behaviour of the DOM elements
+    - These are also called style directive
+    - eg ngClass, ngStyle
 
