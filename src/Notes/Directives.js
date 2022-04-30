91377bf778bd30119795659b3e124752cc7a6d87
Venkat <vbonam@kent.edu>
2022-05-01 00:13:13 +0530

    updated directive notes for ng switch

--- src/Notes/Directives.js
+++ src/Notes/Directives.js
@@ -7,17 +7,22 @@
 
 - There are 3 different kinds of directives
 
-1)Component Directive
+1) Component Directive
     - These are used in the main class
     - They contains the details how component should be processed
-    
-2)Structual Directives
-    - These are used to mainpulate and change the structure
-    - These are preccded with * sign
-    - eg ngIF, ngFor
 
-3)Attribure Directives
+2) Structual Directives
+    - These are used to mainpulate and change the structure
+        - These are preccded with * sign
+        - eg ngIF, ngFor
+
+3) Attribure Directives
     - These are used to change the look and behaviour of the DOM elements
-    - These are also called style directive
-    - eg ngClass, ngStyle
+        - These are also called style directive
+            - eg ngClass, ngStyle
+
+NgSwitch -
+    it actually comprised of two directives, one is structural directive and another is attriburte directive.
+
+
 
