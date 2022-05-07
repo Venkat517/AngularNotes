8fe8a754887dbb9f563cb0f4fbb0e668c4ae6e75
Venkat517 <vbonam@kent.edu>
2022-05-08 02:50:45 +0530

    Update Service.ts

--- src/Notes/Service.ts
+++ src/Notes/Service.ts
@@ -1,1 +1,17 @@
+// Creating a Service 
+
+Ng g s service_name
+
+// Register the service as a provider in the app.module.ts
+
+Provider: [PostService]
+
+// Inject http class in the constructor
+
+constructor(private http: HttpClient) { }
+
+// import that relavant class
+
+import { HttpClient } from '@angular/common/http';
+
 
