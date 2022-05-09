2752d4f00b4cbddf3c819e8d78ad9127e198f58f
Venkat517 <vbonam@kent.edu>
2022-05-09 16:11:23 +0530

    Update Error.ts

--- src/Notes/Error.ts
+++ src/Notes/Error.ts
@@ -1,1 +1,25 @@
+// Types of errors //
 
+There are 2 kinds of errors:
+  - Unexpected Errors
+  - Expected Errors
+  
+Unexpected Errors
+  - Server is offline
+  - Network is down
+  - unhandled exceptions
+  
+Expected Errors
+  - Not found error (404)
+  - Bad Request error (400)
+
+// Handling unexpected errors
+
+this.service.getPosts()
+    .subscribe(data => {
+        this.posts = data
+        console.log(data);       // logging an error
+     }, err => {
+        alert('An unexpected error occurred');
+        console.log(err);
+});
