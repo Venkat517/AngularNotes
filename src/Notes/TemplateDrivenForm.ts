c0aabd4bb55d21b1ddc4684ac2b2d59f5d4657d4
Venkat517 <vbonam@kent.edu>
2022-05-23 01:30:51 +0530

    Update and rename TemplateDrivenForm.js to TemplateDrivenForm.ts

---
+++ src/Notes/TemplateDrivenForm.ts
@@ -0,0 +1,16 @@
+
+// Building a bootstrap form //
+
+<form>
+    <div class="form-group">
+        <label for="firstName">First Name</label>
+        <input id="firstName" type="text" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="comment">Comment</label>
+        <input id="comment" type="text-area" class="form-control" cols="30" rows="10">
+    </div>
+      
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
