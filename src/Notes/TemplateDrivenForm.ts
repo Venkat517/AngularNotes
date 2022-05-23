527eea576e9e9dec135505f26a2f3827f188a3c6
Venkat517 <vbonam@kent.edu>
2022-05-23 13:50:42 +0530

    Update TemplateDrivenForm.ts

--- src/Notes/TemplateDrivenForm.ts
+++ src/Notes/TemplateDrivenForm.ts
@@ -14,3 +14,18 @@
       
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+
+// Types of forms //
+
+we need to create a FORM GROUP object to the form and FORM CONTROL object for the each input field in the form
+
+// Two ways to create the control objects //
+
+1) Directives (Template Driven): Angular will create these implicitilly under the hood
+2) Code (Reactive): Explicitly creating the control objects.
+
+
+
+
+
+
